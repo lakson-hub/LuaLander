918c3b9ba05ccf5724f417fc9ac8131a5e623711
Lakson <lazarmilivojevic0306@gmail.com>
2026-05-20 12:07:41 +0200

    added Lander thruster visuals

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Lander : MonoBehaviour {
+
+    public event EventHandler OnUpForce;
+    public event EventHandler OnRightForce;
+    public event EventHandler OnLeftForce;
+    public event EventHandler OnBeforeForce;
     
     private Rigidbody2D landerRigidbody2D;
     
@@ -11,19 +16,24 @@ public class Lander : MonoBehaviour {
     }
 
     private void FixedUpdate() {
+        OnBeforeForce?.Invoke(this, EventArgs.Empty);
+        
         if (Keyboard.current.upArrowKey.isPressed) {
             float force = 700f;
             landerRigidbody2D.AddForce(transform.up * (force * Time.deltaTime));
+            OnUpForce?.Invoke(this, EventArgs.Empty);
         }
         
         if (Keyboard.current.leftArrowKey.isPressed) {
             float turnSpeed = +100f;
             landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+            OnLeftForce?.Invoke(this, EventArgs.Empty);
         }
         
         if (Keyboard.current.rightArrowKey.isPressed) {
             float turnSpeed = -100f;
             landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+            OnRightForce?.Invoke(this, EventArgs.Empty);
         }
     }
 
