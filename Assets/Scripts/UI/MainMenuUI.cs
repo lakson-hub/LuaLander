d8015512261fdebb8921ecc00d184cc891f34759
Lakson <lazarmilivojevic0306@gmail.com>
2026-05-29 12:54:42 +0200

    added ability to go back to main menu from paused state

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -8,6 +8,8 @@ public class MainMenuUI : MonoBehaviour {
     [SerializeField] private Button quitButton;
 
     private void Awake() {
+        Time.timeScale = 1f;
+        
         playButton.onClick.AddListener(() => {
             GameManager.ResetStaticData();
             SceneLoader.LoadScene(SceneLoader.Scene.GameScene);
--- Assets/Scripts/UI/PausedUI.cs
+++ Assets/Scripts/UI/PausedUI.cs
@@ -5,10 +5,15 @@ using UnityEngine.UI;
 public class PausedUI : MonoBehaviour {
 
     [SerializeField] private Button resumeButton;
+    [SerializeField] private Button mainMenuButton;
 
     private void Awake() {
         resumeButton.onClick.AddListener(() => {
             GameManager.Instance.UnpauseGame();
+        });
+        
+        mainMenuButton.onClick.AddListener(() => {
+            SceneLoader.LoadScene(SceneLoader.Scene.MainMenuScene);
         });
     }
 
