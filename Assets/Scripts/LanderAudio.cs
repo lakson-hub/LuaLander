14dafa7410d705f7fcc2040a0fbd08e668ffcf6a
Lakson <lazarmilivojevic0306@gmail.com>
2026-05-30 15:53:21 +0200

    added Lander thruster sound effects

---
+++ Assets/Scripts/LanderAudio.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+public class LanderAudio : MonoBehaviour {
+
+    [SerializeField] private AudioSource thrusterAudioSource;
+
+    private Lander lander;
+
+    private void Awake() {
+        lander = GetComponent<Lander>();
+    }
+
+    private void Start() {
+        lander.OnBeforeForce += Lander_OnBeforeForce;
+        lander.OnUpForce += Lander_OnUpForce;
+        lander.OnRightForce += Lander_OnRightForce;
+        lander.OnLeftForce += Lander_OnLeftForce;
+        
+        thrusterAudioSource.Pause();
+    }
+
+    private void Lander_OnLeftForce(object sender, EventArgs e) {
+        if (!thrusterAudioSource.isPlaying) {
+            thrusterAudioSource.Play();
+        }
+    }
+
+    private void Lander_OnRightForce(object sender, EventArgs e) {
+        if (!thrusterAudioSource.isPlaying) {
+            thrusterAudioSource.Play();
+        }
+    }
+
+    private void Lander_OnUpForce(object sender, EventArgs e) {
+        if (!thrusterAudioSource.isPlaying) {
+            thrusterAudioSource.Play();
+        }
+    }
+
+    private void Lander_OnBeforeForce(object sender, EventArgs e) {
+        thrusterAudioSource.Pause();
+    }
+}
